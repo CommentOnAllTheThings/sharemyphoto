ed338418418611a6b4e6d8c3da20114d1f6596d9
Timothy Pham <tim.pham@msn.com>
2015-06-17 20:29:11 -0400

    Added test routes.

--- html/smp/app/Http/routes.php
+++ html/smp/app/Http/routes.php
@@ -14,3 +14,35 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/show/{page}', function ($page) {
+    return 'Showing Page '.$page;
+});
+
+Route::get('/upload', function(){
+	return 'Uploader';
+});
+
+Route::get('/delete', function(){
+	return 'Delete';
+});
+
+Route::post('/delete', function(){
+	return 'POST Delete';
+});
+
+Route::get('/view/{id}', function($id){
+	return 'Viewing '.$id;
+});
+
+Route::get('/login', function(){
+	return 'Login Form';
+});
+
+Route::post('/login', function(){
+	return 'Authenticating...';
+});
+
+Route::get('/logout', function(){
+	return 'Logout';
+});
