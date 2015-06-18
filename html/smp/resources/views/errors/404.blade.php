4500024328f9c7b9e1951b6204c1902b5140e3d0
Timothy Pham <tim.pham@msn.com>
2015-06-17 20:54:10 -0400

    Added reference to Bootstrap and jQuery in public/thirdparty.

--- html/smp/resources/views/errors/404.blade.php
+++ html/smp/resources/views/errors/404.blade.php
@@ -1,52 +1,21 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
         <title>ShareMyPho.to Page Not Found</title>
 
-        <link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+        <!-- Load jQuery -->
+        <script type="text/javascript" src="/thirdparty/jquery/jquery.min.js"></script>
 
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                color: #B0BEC5;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 72px;
-                margin-bottom: 40px;
-                padding-left: 20px;
-                padding-right: 20px;
-            }
-        </style>
+        <!-- Load Bootstrap -->
+        <link href='/thirdparty/bootstrap/css/bootstrap.min.css' rel='stylesheet' type='text/css'>
+        <script type="text/javascript" src="/thirdparty/bootstrap/js/bootstrap.min.js"></script>
     </head>
     <body>
         <div class="container">
-            <div class="content">
-                <div class="title">
-                    We're sorry, we couldn't find the image or page you were looking for.<br/>
-                    Perhaps it was deleted?
-                </div>
-                <a href="/">Return to Home</a>
+            <div class="text-center">
+                <h1>We're sorry, we couldn't find the image or page you were looking for.</h1>
+                <h2>Perhaps it was deleted?</h2>
+                <a href="/" class="btn btn-primary" role="button">Return to Home</a>
             </div>
         </div>
     </body>
