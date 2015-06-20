6afa1a8d0452c414454f5957c18b51ede6df9f00
Timothy Pham <tim.pham@msn.com>
2015-06-19 21:23:38 -0400

    Added style.css. Added upload form markup.

--- html/smp/resources/views/uploader.blade.php
+++ html/smp/resources/views/uploader.blade.php
@@ -1,8 +1,38 @@
 @extends('layouts.master')
 @section('pagetitle', 'Upload Image')
 @section('headerincludes')
-	<script type="text/javascript" src="/smp/upload.js"></script>
+<link href="/smp/style.css" rel="stylesheet" type="text/css">
+<script type="text/javascript" src="/smp/upload.js"></script>
+<script type="text/javascript">
+	// Add the CSRF Token
+	csrf_token = "{!! csrf_field() !!}";
+</script>
 @stop
 @section('content')
-	Uploader
+	<div class="container">
+		<form id="upload-form" onsubmit="return false;" action="#">
+			<!-- Image Title -->
+			<div id="upload-title-group" class="form-group">
+				<label for="upload-title">Image Title*</label>
+				<input type="text" id="upload-title" name="upload-title" class="form-control" placeholder="Title">
+				<p id="upload-title-validation" class="validation-error"></p>
+			</div>
+
+			<!-- Image Description -->
+			<div id="upload-description-group" class="form-group">
+				<label for="upload-description">Image Description</label>
+				<textarea id="upload-description" name="upload-description" class="form-control" placeholder="Description" rows="3"></textarea>
+			</div>
+
+			<!-- Image File -->
+			<div id="upload-description-group" class="form-group">
+				<label for="upload-file">Image File*</label>
+				<input type="file" id="upload-file" name="upload-file">
+				<p id="upload-file-validation" class="validation-error"></p>
+			</div>
+
+			<!-- Upload Button -->
+			<button type="submit" class="btn btn-success" id="upload-button">Upload</button>
+		</form>
+	</div>
 @stop
